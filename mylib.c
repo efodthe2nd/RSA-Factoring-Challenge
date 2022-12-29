b571f6842561376f7b1588f524927325a94bbc96
efodthe2nd <davidefod@gmail.com>
2022-12-29 17:21:57 +0000

    initial commit

---
+++ mylib.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * trial_division - divides an int by 2
+ * @n: int to be divided
+ *
+ * Return: nothing
+ */
+
+int trial_division(int n)
+{
+	n = n / 2;
+	printf("%d", n);
+	return (n);
+}
+
